cd45b3f0d769c5a533270cce4444ac3332baf953
vladi642 <vladisto05@gmail.com>
2023-11-23 12:47:38 +0200

    follow cursor
    (неработи както трябва)

--- Assets/Scipts/PlayerMovement.cs
+++ Assets/Scipts/PlayerMovement.cs
@@ -7,27 +7,27 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
 
     public Rigidbody2D rb;
-    public Animator animator;
+    public Camera cam;
 
     Vector2 movement;
+    Vector2 mousePos;
 
 
     // Update is called once per frame
     void Update()
     {
-        // Input 
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
-        animator.SetFloat("Horizontal", movement.x);
-        animator.SetFloat("Vertical", movement.y);
-        animator.SetFloat("Speed", movement.sqrMagnitude);
-
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
 
     void FixedUpdate()
     {
-        // movement
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
     }
 }
